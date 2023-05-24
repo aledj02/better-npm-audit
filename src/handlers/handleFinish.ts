a02f89a2b3056448e57c591b035ff0fb856e1533
Alexis Beutke <alexis.beutke@naranja.com>
2023-05-24 13:56:37 -0300

    Add json report file.

--- src/handlers/handleFinish.ts
+++ src/handlers/handleFinish.ts
@@ -1,6 +1,7 @@
 import { AuditLevel } from 'src/types';
 import { printSecurityReport } from '../utils/print';
 import { processAuditJson, handleUnusedExceptions } from '../utils/vulnerability';
+import * as fs from 'fs';
 
 /**
  * Process and analyze the NPM audit JSON
@@ -28,6 +29,33 @@ export default function handleFinish(jsonBuffer: string, auditLevel: AuditLevel,
   // Print the security report
   if (report.length) {
     printSecurityReport(report);
+
+    // Generate json file
+    const cleanArray = report.map((innerArray) => {
+      return innerArray.map((item) => {
+        return item.replace(/\x1B\[\d+m/g, '');
+      });
+    });
+
+    const jsonKeyValue = cleanArray.map((item) => {
+      return {
+        id: item[0],
+        package: item[1],
+        title: item[2],
+        path: item[3],
+        severity: item[4],
+        guide: item[5],
+        expired: item[6],
+      };
+    });
+
+    const jsonContent = JSON.stringify(jsonKeyValue, null, 2);
+
+    saveJsonReport(jsonContent);
+  } else {
+    const emptyArray: unknown[] = [];
+    const jsonContent = JSON.stringify(emptyArray, null, 2);
+    saveJsonReport(jsonContent);
   }
 
   // Handle unused exceptions
@@ -42,5 +70,20 @@ export default function handleFinish(jsonBuffer: string, auditLevel: AuditLevel,
     // Happy happy, joy joy
     console.info('🤝  All good!');
     process.exit(0);
+  }
+}
+
+/**
+ * Create file NPM audit JSON
+ * @param  {String} jsonContent        JSON object to save
+ */
+function saveJsonReport(jsonContent: string) {
+  try {
+    if (!fs.existsSync('.sast')) {
+      fs.mkdirSync('.sast');
+    }
+    fs.writeFileSync('.sast/better_npm_report.json', jsonContent, 'utf8');
+  } catch (err) {
+    console.error('Error al exportar el reporte a json:', err);
   }
 }
